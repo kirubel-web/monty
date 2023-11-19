d205cb06f50b278ac91237260e75c0ab6794aa20
kirubel-web <kirubelalemu119@gmail.com>
2023-11-19 02:43:11 -0800

    task fix 2

--- task_1.c
+++ task_1.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * pint - prints the value at the top of the stack
- * @stack: pointer to the top of the stack
- * @line_number: line number of the opcode
+ * pint - show value top of the stack
+ * @stack: top of the stack
+ * @line_number: line number
 */
 void pint(stack_t **stack, unsigned int line_number)
 {
